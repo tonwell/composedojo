2e1f981ec1b3dd23c820324c3578ad26e5115edb
tonwell <wellington.pbi10@gmail.com>
2023-04-03 11:40:43 -0300

    changing mocked values

--- app/src/main/java/com/example/composedojo/domain/GiftCardModel.kt
+++ app/src/main/java/com/example/composedojo/domain/GiftCardModel.kt
@@ -12,10 +12,10 @@ data class GiftCardModel(
 
 val mockedMarioKartGiftCard = GiftCardModel(
     status = "Desbloqueado",
-    orderId = "02-757368298",
-    cardId = "3086559990549698671",
+    orderId = "12-123456789",
+    cardId = "1234567890987654321",
     image = "https://content.blackhawknetwork.com/gcmimages/product/icon/101769.jpg",
-    info = "Mario Kart 8 eGift (GiftCard: aa2b)",
-    pinCode = "b15b86h941bgx5xl",
+    info = "Mario Kart 8 eGift (GiftCard: aabc)",
+    pinCode = "ab12cd34ef56gh78ij9",
     retrieveInstructions = "dialog com textos pegos sabe-se lá aonde"
 )
